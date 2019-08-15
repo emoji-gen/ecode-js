4e0569d260dedbc310213c82ba9179d7c936aaec
Pine Mizune <pinemz@gmail.com>
2019-08-15 17:53:35 +0900

    fix: add webpack

--- karma.conf.js
+++ karma.conf.js
@@ -60,10 +60,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
+    singleRun: true,
     concurrency: Infinity
   })
 }
