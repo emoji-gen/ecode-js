df2c50edd3e08514d4130d50a9c1ab5de5895710
Pine Mizune <pinemz@gmail.com>
2019-08-21 23:45:23 +0900

    test: test illegal version number

--- test/ecode-decoder-test.js
+++ test/ecode-decoder-test.js
@@ -2,6 +2,7 @@
 
 const { expect } = require('chai')
 const { EcodeDecoder } = require('../index')
+const base64 = require('../lib/base64')
 
 describe('EcodeDecoder', () => {
   describe('decode', () => {
@@ -23,6 +24,28 @@ describe('EcodeDecoder', () => {
     it('should fail to decode due to illegal length', () => {
       const ecodeDecoder = new EcodeDecoder()
       expect(() => { ecodeDecoder.decode('') }).to.throw(Error, 'Illegal byte length 0')
+    })
+    it('should fail to decode due to illegal version', () => {
+      const ecodeDecoder = new EcodeDecoder()
+      const ecode = base64.encode([
+        0xf4, // Version:4, Locale:4
+        0x0d, // Flags:6, Align:2
+        0x21, // Size:4, Format:4
+        0xcf, // FontId:8
+        0x12, // ForegroundColor_R:8
+        0x34, // ForegroundColor_G:8
+        0x56, // ForegroundColor_B:8
+        0x78, // ForegroundColor_A:8
+        0x9a, // BackgroundColor_R:8
+        0xbc, // BackgroundColor_G:8
+        0xde, // BackgroundColor_B:8
+        0xf0, // BackgroundColor_A:8
+        0x61, // Text:8
+        0x62, // Text:8
+        0x0a, // Text:8
+        0x63, // Text:8
+      ])
+      expect(() => { ecodeDecoder.decode(ecode) }).to.throw(Error, 'Illegal version 16')
     })
   })
 })
