ad0934e8bfb2a1dd81bcedf9d7abfd29b115d435
Pine Mizune <pinemz@gmail.com>
2019-08-21 23:48:48 +0900

    test: test due to illegal locale ID

--- test/ecode-decoder-test.js
+++ test/ecode-decoder-test.js
@@ -47,5 +47,27 @@ describe('EcodeDecoder', () => {
       ])
       expect(() => { ecodeDecoder.decode(ecode) }).to.throw(Error, 'Illegal version 16')
     })
+    it('should fail to decode due to illegal locale ID', () => {
+      const ecodeDecoder = new EcodeDecoder()
+      const ecode = base64.encode([
+        0x0f, // Version:4, Locale:4
+        0x0d, // Flags:6, Align:2
+        0x21, // Size:4, Format:4
+        0xcf, // FontId:8
+        0x12, // ForegroundColor_R:8
+        0x34, // ForegroundColor_G:8
+        0x56, // ForegroundColor_B:8
+        0x78, // ForegroundColor_A:8
+        0x9a, // BackgroundColor_R:8
+        0xbc, // BackgroundColor_G:8
+        0xde, // BackgroundColor_B:8
+        0xf0, // BackgroundColor_A:8
+        0x61, // Text:8
+        0x62, // Text:8
+        0x0a, // Text:8
+        0x63, // Text:8
+      ])
+      expect(() => { ecodeDecoder.decode(ecode) }).to.throw(Error, 'Illegal locale ID 15')
+    })
   })
 })
