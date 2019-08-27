80e73e7678b4be707763d0ff7e0d73dd523ed698
Pine Mizune <pinemz@gmail.com>
2019-08-27 18:14:09 +0900

    test: add tests

---
+++ test/base64-test.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const { expect } = require('chai')
+const base64 = require('../lib/base64')
+
+describe('base64', () => {
+  describe('decode', () => {
+    it('should encode text', () => {
+      expect(base64.encode(Uint8Array.from([ 0x61, 0x62, 0x63 ]))).to.equal('YWJj')
+      expect(base64.encode(Uint8Array.from([ 0x31, 0x32, 0x33, 0x34, 0x35 ]))).to.equal('MTIzNDU')
+      expect(base64.encode(Uint8Array.from([ 0x7e, 0x7e, 0x7e, 0x7e ]))).to.equal('fn5-fg')
+    })
+  })
+  describe('encode', () => {
+    it('should decode text', () => {
+      expect(base64.decode('YWJj')).to.deep.equal(Uint8Array.from([ 0x61, 0x62, 0x63 ]))
+      expect(base64.decode('MTIzNDU')).to.deep.equal(Uint8Array.from([ 0x31, 0x32, 0x33, 0x34, 0x35 ]))
+      expect(base64.decode('fn5-fg')).to.deep.equal(Uint8Array.from([ 0x7e, 0x7e, 0x7e, 0x7e ]))
+    })
+  })
+})
